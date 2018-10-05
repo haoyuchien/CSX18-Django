90a44a312d170cb13b144d233b354916fc44e61e
HaoYu Chien <b06104048@ntu.edu.tw>
2018-10-05 12:10:59 +0800

    Week 4

---
+++ guestbook/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class TourDates(models.Model):
+    city = models.CharField(max_length=20, blank=False)
+    venue = models.CharField(max_length=50, blank=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.city + " " + self.venue + " " + self.date

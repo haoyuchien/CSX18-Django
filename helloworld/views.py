d14d9c278f865b260f0b58ce8c1b1705e48911ec
spark131072 <spark131072@gmail.com>
2018-10-14 03:05:08 +0800

    131018_week5_02

--- helloworld/views.py
+++ helloworld/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from guestbook.models import TourDates
 import random
+from guestbook.models import TextMessage
 
 
 def index(request):
